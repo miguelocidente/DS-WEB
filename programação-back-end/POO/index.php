cdcbde4fc6f18f1c54e276031e2d50c100b48207
MIGUEL DOS SANTOS PÁSCOLI <miguel.pascoli2@senaisp.edu.br>
2026-02-03 14:05:48 -0300

    Primeiro commit de 2026

---
+++ programação-back-end/POO/index.php
@@ -0,0 +1,3 @@
+<?php 
+
+echo "<h1>Aulas de Programação Orientada a Objetos</h1>";
---
+++ programação-back-end/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+echo "<h1>Pagina Principal do meu servidor</h1>";
+?>
+
+<a href="/POO/">Aula de POO</a>
